c00f67d4860dd8f09246cbfec0f3f0a62578522e
Sebastian <arenafortni12@gmail.com>
2026-04-19 09:58:17 +0200

    feat: añade búsqueda de productos

--- src/Main.java
+++ src/Main.java
@@ -7,3 +7,18 @@ public class Main {
 
     }
 }
+
+public static void buscarProducto(ArrayList<String> productos, String nombreBuscado) {
+ boolean encontrado = false;
+ for (String producto : productos) {
+ if (producto.equalsIgnoreCase(nombreBuscado)) {
+ encontrado = true;
+ break;
+ }
+ }
+ if (encontrado) {
+ System.out.println("Producto encontrado en el inventario.");
+ } else {
+ System.out.println("El producto no existe en el inventario.");
+ }
+}
